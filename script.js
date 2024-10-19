0358defda566418c8200b7ca91ec64ce82cee758
somesh-opps <somesh.sahoo@outlook.com>
2024-10-19 23:45:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+document.getElementById('about-dev').addEventListener('click', function() {
+    document.getElementById('page2').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+
+document.getElementById('education-dev').addEventListener('click', function() {
+    document.getElementById('education').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+
+document.getElementById('events-dev').addEventListener('click', function() {
+    document.getElementById('EventsAttended').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+
+document.getElementById('membership-dev').addEventListener('click', function() {
+    document.getElementById('membership').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+
+document.getElementById('contact-dev').addEventListener('click', function() {
+    document.getElementById('subpagex').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+document.getElementById('iconfront').addEventListener('click', function() {
+    document.getElementById('page1').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
---
+++ scriptForContact.js
@@ -0,0 +1,45 @@
+// first page contacts
+
+document.getElementById('email').addEventListener('click', function() {
+    window.location.href = 'mailto:smeshkumar.sahoo2024@uem.edu.in';
+});
+
+
+document.getElementById('tel').addEventListener('click', function() {
+    window.location.href = 'tel:+91 9614175335';
+}); 
+
+document.getElementById('linkedin').addEventListener('click', function() {
+    window.open('https://www.linkedin.com/in/somesh-kumar-sahoo-900803320', '_blank');
+});
+
+document.getElementById('github').addEventListener('click', function() {
+    window.open('https://github.com/somesh-opps', '_blank');
+});
+
+document.getElementById('x').addEventListener('click', function() {
+    window.open('https://x.com/somesh659', '_blank');
+});
+
+
+// links for contact adress page
+document.getElementById('email2').addEventListener('click', function() {
+    window.location.href = 'mailto:smeshkumar.sahoo2024@uem.edu.in';
+});
+
+
+document.getElementById('tel2').addEventListener('click', function() {
+    window.location.href = 'tel:+91 9614175335';
+}); 
+
+document.getElementById('linkedin2').addEventListener('click', function() {
+    window.open('https://www.linkedin.com/in/somesh-kumar-sahoo-900803320', '_blank');
+});
+
+document.getElementById('github2').addEventListener('click', function() {
+    window.open('https://github.com/somesh-opps', '_blank');
+});
+
+document.getElementById('x2').addEventListener('click', function() {
+    window.open('https://x.com/somesh659', '_blank');
+});
